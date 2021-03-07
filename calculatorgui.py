1a0bcf2c31505021bf04123e52397c5aa0ff0fe4
Adithya Vijay K <adithyavj@gmail.com>
2021-03-07 19:50:02 +0530

    frame for buttons created

--- calculatorgui.py
+++ calculatorgui.py
@@ -9,14 +9,21 @@ win.title("Calculator")
 # 'StringVar()' :It is used to get the instance of input field
 input_text = StringVar()
 
-# Let us creating a frame for the input field
+"""  USER INPUT FRAME  """
+# Creating a frame for the input field
 input_frame = Frame(win, width=312, height=50, bd=0, highlightbackground="black", highlightcolor="black",
                     highlightthickness=2)
 input_frame.pack(side=TOP)
 
-# Let us create a input field inside the 'Frame'
-input_field = Entry(input_frame, font=('arial', 18, 'bold'), textvariable=input_text, width=50, bg="#FFFFFF", bd=0,
+# creating an input field inside the 'Frame' for reading the user input
+input_field = Entry(input_frame, font=('arial', 20, 'bold'), textvariable=input_text, width=50, bg="#FFFFFF", bd=0,
                     justify=RIGHT)
 input_field.grid(row=0, column=0)
-input_field.pack()
+input_field.pack(ipady=10)  # 'ipady' is internal padding to increase the height of input field
+
+"""  BUTTONS FRAME  """
+# Let us creating another 'Frame' for the button below the 'input_frame'
+btns_frame = Frame(win, width=312, height=272.5, bg="grey")
+btns_frame.pack()
+
 win.mainloop()
