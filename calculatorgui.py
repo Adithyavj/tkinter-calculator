1b2cba06a4f6e9222ba537261646db3eef30bd3c
Adithya Vijay K <adithyavj@gmail.com>
2021-03-07 19:44:35 +0530

    Creating user interface, input field created

--- calculatorgui.py
+++ calculatorgui.py
@@ -4,4 +4,19 @@ win = Tk()  # This is to create a basic window
 win.geometry("312x324")  # this is for the size of the window
 win.resizable(0, 0)  # this is to prevent from resizing the window
 win.title("Calculator")
+## Next we create the GUI of calculator
+
+# 'StringVar()' :It is used to get the instance of input field
+input_text = StringVar()
+
+# Let us creating a frame for the input field
+input_frame = Frame(win, width=312, height=50, bd=0, highlightbackground="black", highlightcolor="black",
+                    highlightthickness=2)
+input_frame.pack(side=TOP)
+
+# Let us create a input field inside the 'Frame'
+input_field = Entry(input_frame, font=('arial', 18, 'bold'), textvariable=input_text, width=50, bg="#FFFFFF", bd=0,
+                    justify=RIGHT)
+input_field.grid(row=0, column=0)
+input_field.pack()
 win.mainloop()
